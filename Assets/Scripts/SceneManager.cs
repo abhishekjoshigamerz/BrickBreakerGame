708c817e9291a1cbcb7323016918690e26544afc
abhishekjoshigamerz <abhishekjoshicctian1@gmail.com>
2021-07-12 21:35:31 +0530

    This is first commit for the project

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManager : MonoBehaviour
+{
+    private int  firstSceneIndex = 0;
+    private int SceneIndex = 0;
+    public void StartGame(){
+        SceneIndex++;
+        firstSceneIndex++;
+        //had to do this because scenemanager and unityscene manager had same names and I did not took care of that at that time. so did this
+        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(firstSceneIndex);
+        firstSceneIndex=0;
+    }
+    
+    public void QuitGame(){
+        Debug.Log("Game Quit!");
+        Application.Quit();
+    }
+}
